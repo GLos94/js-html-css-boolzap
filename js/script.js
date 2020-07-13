030a27146d5ed4619fc4e3083e1368a4f8f69e21
Giulia <giulialosito94@gmail.com>
2020-07-13 16:28:47 +0200

    Prova js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-// $(document).ready(function() {
-//
-//
-//
-//
-//
-//
-//
-// });
+function scriviMes(){
+  $('#invia').click(inviaMes)
+}
+
+
+function inviaMes() {
+  var mes;
+  mes = $("#scrivi").val();
+  console.log(mes);
+  $(mes).clone();
+  $('p.testo').append(mes);
+
+}
+
+
+function init() {
+  scriviMes();
+  inviaMes();
+}
+
+$(document).ready(init);
